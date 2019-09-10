c44ab989f09a1f9df42e1661bf5c92983798d7f8
OctavioPalacios <ocpl2710@gmail.com>
2019-09-09 19:11:49 -0500

    Ejercicio 3 Node JS

---
+++ ejercicio3.js
@@ -0,0 +1,12 @@
+usuarioIngresada = "octabod"
+contraseñaIngresada = "octa10"
+usuarioGuardada = "octabomd"
+contraseñaGuardada = "octa10"
+
+
+if(usuarioIngresada === usuarioGuardada && contraseñaIngresada === contraseñaGuardada){
+    console.log('Refrigerador Abierto')
+}
+else{
+    console.log('Usuario o Contraseña erroneos')
+}
