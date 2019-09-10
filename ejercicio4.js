013e781d1c4e5806c71e6ce68211ff96d629bea4
OctavioPalacios <ocpl2710@gmail.com>
2019-09-09 19:12:00 -0500

    Ejercicio 4 Node JS

---
+++ ejercicio4.js
@@ -0,0 +1,13 @@
+calificacion1 = 8.5
+calificacion2 = 8.5
+calificacion3 = 8.5
+promedioMeta = 8.5
+promedioObtenido = (calificacion1 + calificacion2 +calificacion3) / 3
+
+
+if(promedioObtenido >= promedioMeta){
+    console.log('Felicidades Toma El Regalo')
+}
+else{
+    console.log('Estas Castigado')
+}
